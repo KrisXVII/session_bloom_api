9bea5f2f75dba7fa68bd465c49503e6a03460e91
Cristian Radulescu <kristianradulescu@gmail.com>
2026-03-21 12:52:20 +0100

    ci/cd: test push

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
-
+import os
 from flask import Flask
+from dotenv import load_dotenv
 
 def create_app():
 	app = Flask(__name__)
 
+	env = os.getenv("FLASK_ENV")
+
+	# Register blueprints (like Rails routes.rb)
+	# from app.routes.user_routes import user_bp
+	# from app.routes.session_routes import session_bp
+
+	# app.register_blueprint(user_bp, url_prefix='/users')
+	# app.register_blueprint(session_bp, url_prefix='/sessions')
 
 	@app.route('/')
 	def hello_world():  # put application's code here
