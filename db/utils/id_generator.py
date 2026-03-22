5c7894421be28433fcb89a37f0b34ab489e82348
Cristian Radulescu <kristianradulescu@gmail.com>
2026-03-22 12:56:29 +0100

    fix: id generator

--- db/utils/id_generator.py
+++ db/utils/id_generator.py
@@ -3,42 +3,44 @@ from datetime import datetime
 TODAY = datetime.utcnow().date()
 
 class IDGenerator:
-	def __init__(self):
-		self.counter = 0
-		self.last_date = datetime.utcnow().date()
+	_counter = 0
+	_last_date = datetime.utcnow().date()
 
-	def generate_user_uid(self):
+	@classmethod
+	def generate_user_uid(cls):
 
-		if TODAY > self.last_date:
-			self.counter = 0
-			self.last_date = TODAY
+		if TODAY > cls._last_date:
+			cls._counter = 0
+			cls._last_date = TODAY
 
 		date_part = TODAY.strftime('%y%m%d')
-		counter_part = f"{self.counter:06d}"
-		self.counter += 1
+		counter_part = f"{cls._counter:06d}"
+		cls._counter += 1
 
 		return f"USR_{date_part}{counter_part}"
 
-	def generate_session_uid(self):
+	@classmethod
+	def generate_session_uid(cls):
 
-		if TODAY > self.last_date:
-			self.counter = 0
-			self.last_date = TODAY
+		if TODAY > cls._last_date:
+			cls._counter = 0
+			cls._last_date = TODAY
 
 		date_part = TODAY.strftime('%y%m%d')
-		counter_part = f"{self.counter:06d}"
-		self.counter += 1
+		counter_part = f"{cls._counter:06d}"
+		cls._counter += 1
 
 		return f"SES_{date_part}{counter_part}"
 
-	def generate_subsession_uid(self):
+	@classmethod
+	def generate_subsession_uid(cls):
 
-		if TODAY > self.last_date:
-			self.counter = 0
-			self.last_date = TODAY
+		if TODAY > cls._last_date:
+			cls._counter = 0
+			cls._last_date = TODAY
 
 		date_part = TODAY.strftime('%y%m%d')
-		counter_part = f"{self.counter:06d}"
-		self.counter += 1
+		counter_part = f"{cls._counter:06d}"
+		cls._counter += 1
 
 		return f"SUB_{date_part}{counter_part}"
