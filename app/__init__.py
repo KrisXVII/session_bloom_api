bc29ea39e02437088dd4291219959671fc3dee12
Cristian Radulescu <kristianradulescu@gmail.com>
2026-03-22 11:59:50 +0100

    fix: changed env variables management on app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,28 @@
 import os
 from flask import Flask
+from db.base import db
 from dotenv import load_dotenv
 
 def create_app():
 	app = Flask(__name__)
-
+	load_dotenv()
 	env = os.getenv("FLASK_ENV")
 
-	# Register blueprints (like Rails routes.rb)
-	# from app.routes.user_routes import user_bp
+	if env == "test":
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+	else:
+		app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+	db.init_app(app)
+
+	# Register blueprints
+	from app.controllers.user_controller import user_bp
 	# from app.routes.session_routes import session_bp
 
-	# app.register_blueprint(user_bp, url_prefix='/users')
+	app.register_blueprint(user_bp, url_prefix='/users')
 	# app.register_blueprint(session_bp, url_prefix='/sessions')
-	# ci comment for test
 
 	@app.route('/')
 	def hello_world():
