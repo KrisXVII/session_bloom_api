17dfa1009a06878232e87525de1774dc8d2095b1
Cristian Radulescu <kristianradulescu@gmail.com>
2026-05-21 18:59:05 +0200

    style: shifted relationships at the bottom of attributes definition

--- app/models/session.py
+++ app/models/session.py
@@ -13,11 +13,12 @@ class Session(BaseModel):
 	id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 	code = db.Column(db.String(20), unique=True, nullable=False)
 	activity = db.Column(db.String(50), nullable=False)
-	user_id = db.Column(db.String(20), db.ForeignKey("users.id"))
-	user = db.relationship("User", back_populates="sessions", lazy=True)
+	user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("users.id"))
 	subsessions = db.relationship("Subsession", back_populates="session", lazy=True, cascade='all, delete-orphan')
 	created_at = db.Column(db.DateTime, default=datetime.utcnow)
 	updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+	user = db.relationship("User", back_populates="sessions", lazy=True)
 
 	def __init__(self, **kwargs):
 		if "code" not in kwargs:
